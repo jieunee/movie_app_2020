7a0975c61e4a41cce852721d2c4498123d338945
Green LEE <green@with.green>
2020-08-22 19:00:19 +0900

    Updates

---
+++ precache-manifest.4050ba1a22df4acba139a9d472a92d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638821c543debf1c4044e1b6aaba838e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "da6ee6bf5307a1ce2b89",
+    "url": "/movie_app_2020/static/css/main.7db549a5.chunk.css"
+  },
+  {
+    "revision": "b0f64fb423232732c18c",
+    "url": "/movie_app_2020/static/js/2.229824db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.229824db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da6ee6bf5307a1ce2b89",
+    "url": "/movie_app_2020/static/js/main.f79a2f81.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
