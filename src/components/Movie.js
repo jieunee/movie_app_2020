bffab00dcfa283ef02bb698507ca4c57bab31bc9
Green LEE <green@realcomes.com>
2020-09-01 17:50:42 +0900

    CSS Change to NES.CSS

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Movie.css';
+//import './Movie.css';
+import 'nes.css/css/nes.min.css';
 
 function Movie({ year, title, summary, poster, genres }) {
     return (
-        <div className="movie">
+        <div className="nes-balloon from-left nes-pointer">
             <img src={poster} alt={title} title={title} />
             <div className="movie__data">
-                <h3 className="movie__title">{title}</h3>
+                <h3 className="nes-text is-success">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
-                <ul className="movie__genres">
+                <ul className="lists">
                     {genres.map((genre, index) => {
-                        return (<li key={index} className="movie__genre">{genre}</li>);
+                        return (<li key={index} className="nes-list is-disc">{genre}</li>);
                     })}
 
                 </ul>
-                <p className="movie__summary">{summary.slice(0, 180)}...</p>
+                <p className="nes-btn is-primary">{summary.slice(0, 180)}...</p>
             </div>
         </div>
     );
